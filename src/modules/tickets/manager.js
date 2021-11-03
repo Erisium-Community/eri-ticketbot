bc947f9ace64139fdb2ca4a362cff1a4d5480467
Maël Debon <45589315+maeldebon@users.noreply.github.com>
2021-11-03 22:50:15 +0100

    Increase channel delete delay

--- src/modules/tickets/manager.js
+++ src/modules/tickets/manager.js
@@ -268,7 +268,7 @@ module.exports = class TicketManager extends EventEmitter {
 
 				setTimeout(async () => {
 					await channel.delete(`Ticket channel closed by ${closer.user.tag}${reason ? `: "${reason}"` : ''}`);
-				}, 5000);
+				}, 600000);
 
 				this.client.log.info(`${closer.user.tag} closed a ticket (${ticket_id})${reason ? `: "${reason}"` : ''}`);
 			} else {
@@ -287,7 +287,7 @@ module.exports = class TicketManager extends EventEmitter {
 
 				setTimeout(async () => {
 					await channel.delete(`Ticket channel closed${reason ? `: "${reason}"` : ''}`);
-				}, 5000);
+				}, 600000);
 
 				this.client.log.info(`A ticket was closed (${ticket_id})${reason ? `: "${reason}"` : ''}`);
 			}
